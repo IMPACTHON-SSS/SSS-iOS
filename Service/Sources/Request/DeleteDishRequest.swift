1c582e652922fb35fe756f75dc22980b1423921d
uuuunseo <jdbstjoo@gmail.com>
2024-01-28 02:16:28 +0900

    :sparkles: :: 음식일기 Request

---
+++ Service/Sources/Request/DeleteDishRequest.swift
@@ -0,0 +1,8 @@
+//
+//  DeleteDishRequest.swift
+//  Service
+//
+//  Created by 정윤서 on 1/28/24.
+//
+
+import Foundation
---
+++ Service/Sources/Request/UploadImageRequest.swift
@@ -0,0 +1,8 @@
+//
+//  UploadImageRequest.swift
+//  Service
+//
+//  Created by 정윤서 on 1/28/24.
+//
+
+import Foundation
