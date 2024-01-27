e1e858af8c5cc8bd83ff54db07d8480c8b3c750a
uuuunseo <jdbstjoo@gmail.com>
2024-01-28 02:18:36 +0900

    :sparkles: :: Service / 이미지 업로드 Request

--- Service/Sources/Request/UploadImageRequest.swift
+++ Service/Sources/Request/UploadImageRequest.swift
@@ -1,8 +1,21 @@
-//
-//  UploadImageRequest.swift
-//  Service
-//
-//  Created by 정윤서 on 1/28/24.
-//
-
 import Foundation
+import Sora
+
+public struct UploadImageRequest: Requestable, Body {
+    public let route = SSSService.path("image")
+    public let method: RequestMethod = .post
+
+    public let body: Body
+
+    public init(body: Body) {
+        self.body = body
+    }
+
+    public struct Body: Encodable {
+        public let file: String
+
+        public init(file: String) {
+            self.file = file
+        }
+    }
+}
