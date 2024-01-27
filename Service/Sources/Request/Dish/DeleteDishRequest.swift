4ca418285aa6f99b760f8d5f07a84a4a072d53b7
uuuunseo <jdbstjoo@gmail.com>
2024-01-28 03:13:15 +0900

    :sparkles: :: Service / 음식일기 수정 Request

--- Service/Sources/Request/Dish/DeleteDishRequest.swift
+++ Service/Sources/Request/Dish/DeleteDishRequest.swift
@@ -4,7 +4,7 @@ import Sora
 public struct DeleteDishRequest: Requestable {
     public var route = SSSService.path("dish", "2")
     public let method: RequestMethod = .delete
-    
+
     init(route: ServiceType) {
         self.route = route
     }
--- Service/Sources/Request/Dish/FetchDishRequest.swift
+++ Service/Sources/Request/Dish/FetchDishRequest.swift
@@ -4,15 +4,20 @@ import Sora
 public struct FetchDishRequest: Requestable, Body {
     public let route = SSSService.path("dish", "2024", "01")
     public let method: RequestMethod = .get
-    
+
     public let body: Body
-    
+
     public init(body: Body) {
         self.body = body
     }
-    
+
     public struct Body: Encodable {
         public let year: Int
         public let month: Int
+
+        public init(year: Int, month: Int) {
+            self.year = year
+            self.month = month
+        }
     }
 }
